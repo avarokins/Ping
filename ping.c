28ad2020d0aff6f40c4b4606ec5a271c8c4c18bb
avarokins <saini7@purdue.edu>
2020-04-16 13:34:21 +0000

    Added reverse DNS lookup.

--- ping.c
+++ ping.c
@@ -8,15 +8,18 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string.h>
-
-
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <unistd.h>
 
 // Global variables and defines here
 
 #define pkt_size 64
 
 int port_no = 0;  // default port number
-int sleep = 1000000;  // Time before pinging again (1s)
+int sleep_time = 1000000;  // Time before pinging again (1s)
 int timeout = 1;   // timeout to recieve reply in seconds
 bool keepalive = true;  // Keeps the ping loop running
 int ttl = 64; // Default ttl
@@ -30,24 +33,63 @@ struct pkt {
   char msg[pkt_size - sizeof(struct icmphdr)];  // message for echo
 };
 
-// Handler for SIGINT (ctrl-C)
+/*
+ * Handler for SIGINT (ctrl-C)
+ */
 void handler( int sig ) {
   keepalive = false;  // End program when ctrl-C
 }
 
-char* dns_lookup () {
 
+/*
+ * Helper function to find ip address from given hostname.
+ */
+char* dns_lookup (char* hostname, struct sockaddr_in* addr) {
 
+  struct hostent* host;
+  char* ip = malloc(NI_MAXHOST * sizeof(char));
 
+  host = gethostbyname(hostname);
+  if ( host == NULL ) {
+    return NULL;
+  } else {
+
+    strcpy(ip, inet_ntoa( *(struct in_addr*)host->h_addr ));
+    addr->sin_port = htons(port_no);
+    addr->sin_family = host->h_addrtype;
+    addr->sin_addr.s_addr = *(long*) host->h_addr;
+  }
+  return ip;
+}
+
+/*
+ * Helper function to perform reverse dns lookup
+ */
+char* rev_lookup ( char* ip ) {
+
+  struct sockaddr_in temp;
+  socklen_t length = sizeof(struct sockaddr_in);
+  char buffer[NI_MAXHOST];
+
+  temp.sin_family = AF_INET;
+  temp.sin_addr.s_addr = inet_addr(ip);
+
+  int ret = getnameinfo((struct sockaddr *) &temp, length, buffer,
+    sizeof(buffer), NULL, 0, NI_NAMEREQD);
+  if ( ret != 0 ) {
+    return NULL;
+  }
+
+  char *rev = malloc (( sizeof (buffer) + 1 ) * sizeof(char));
+  strcpy(rev, buffer);  // Add null terminator
+  return rev;
 }
 
 
 
-
-
-
-
-
+/*
+ * main method
+ */
 int main(int argc, char * argv[]) {
 
   // Socket for ICMP
@@ -56,7 +98,7 @@ int main(int argc, char * argv[]) {
   char* rev_hostname;
 
   struct sockaddr_in addr;  // Struct for internet addresses
-  int addr_len = sizeof(addr_con);
+  int addr_len = sizeof(addr);
   char net_but[NI_MAXHOST];
 
   // Parse arguments
@@ -81,13 +123,29 @@ int main(int argc, char * argv[]) {
   }
 
   // Perform DNS lookup
-  ip = dns_lookup( hostname, &addr)
+  ip = dns_lookup( hostname, &addr);
+  if ( ip == NULL )  {
+    printf("Cannot resolve hostname: %s\n", hostname);
+    return 1;
+  }
 
+  // Perform reverse hostname search
+  rev_hostname = rev_lookup( ip );
+  if ( rev_hostname == NULL ) {
+    printf("Can not perform reverse lookup of hostname!\n");
+  }
 
+  // Header output
+  printf("PING %s (%s) - %d bytes of data.\n", hostname, ip, pkt_size);
 
+  // Open an ICMP socket
+  sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+  if ( sock < 0 ) {
+    printf("Unable to open socket\n");
+    return 1;
+  }
 
-
-
+  // Handle ctrl-C
   signal( SIGINT, handler );
 
   return 0;
